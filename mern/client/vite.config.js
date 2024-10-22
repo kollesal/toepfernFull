1fd7ee5334cdb4654eb24b1ddbed7cd2a0d37eab
Salome Koller <salomejkoller@gmail.com>
2024-10-22 09:18:05 +0200

    test

--- mern/client/vite.config.js
+++ mern/client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Ensure this base URL is correct
+  build: {
+    outDir: 'public',  // Vite builds to 'dist' by default
+  },
 })
