bc4d2155a979a80507d69adf2effe667231a896f
Salome Koller <salomejkoller@gmail.com>
2024-10-22 09:24:46 +0200

    picture

--- mern/client/src/components/RecordList.jsx
+++ mern/client/src/components/RecordList.jsx
@@ -95,7 +95,7 @@ export default function RecordList() {
 <div className="my-4">
         <h3 className="text-lg font-semibold p-4">Forming</h3>
         <video width="600" controls>
-          <source src="src/components/20241020_084356.mp4" type="video/mp4" />
+          <source src="src/assets/20241020_084356.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
@@ -103,7 +103,7 @@ export default function RecordList() {
       <div className="my-4">
         <h3 className="text-lg font-semibold p-4">Footing</h3>
         <video width="600" controls>
-          <source src="src/components/20241021_081329.mp4" type="video/mp4" />
+          <source src="src/assets/20241021_081329.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
@@ -112,7 +112,7 @@ export default function RecordList() {
        <div className="my-4">
         <h3 className="text-lg font-semibold p-4">Form Front</h3>
         <img 
-          src="src/components/Front.jpg" 
+          src="src/assets/Front.jpg" 
           alt="Team Photo" 
           width="600" 
           className="rounded-lg"
@@ -122,7 +122,7 @@ export default function RecordList() {
       <div className="my-4">
         <h3 className="text-lg font-semibold p-4">Form Front</h3>
         <img 
-          src="src/components/Foot.jpg" 
+          src="src/assets/Foot.jpg" 
           alt="Team Photo" 
           width="600" 
           className="rounded-lg"
