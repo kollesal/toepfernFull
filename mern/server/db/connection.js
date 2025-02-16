8e272c8a4cce492337ee8856bfd7cec77fd98775
Salome Koller <salomejkoller@gmail.com>
2025-02-16 19:11:35 +0100

    connection

--- mern/server/db/connection.js
+++ mern/server/db/connection.js
@@ -1,24 +1,28 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const URI = process.env.ATLAS_URI || "";
-const client = new MongoClient(URI, {
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://salomejkoller:f8ZVFal99lKJDiMt@cluster0.1r8kl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
+  }
 });
 
-try {
-  // Connect the client to the server
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
-} catch (err) {
-  console.error(err);
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
-
-let db = client.db("employees");
-
-export default db;
+run().catch(console.dir);
