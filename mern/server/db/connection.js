ea78a6c1691f852b108b8d8606ca23b97f70ae57
Salome Koller <salomejkoller@gmail.com>
2025-02-18 22:48:33 +0100

    connection

--- mern/server/db/connection.js
+++ mern/server/db/connection.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 async function connectDB() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    await client.db("employees").command({ ping: 1 });
     console.log("✅ Connected to MongoDB successfully!");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
